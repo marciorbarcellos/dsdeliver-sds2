8d2bc11d02c469f32854621b5c31582239d13f3c
Marcio Barcellos <marcio.barcellos@outlook.com>
2021-01-11 14:46:58 -0300

    Finish front-web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
 import { OrderLocationData } from './types';
 
 const initialPosition = {
-    lat: -18.9110558,
-    lng: -48.26201
+    lat: -23.4989433,
+    lng: -46.877331
 }
 
 type Place = {
@@ -15,43 +15,43 @@ type Place = {
     position: {
         lat: number;
         lng: number;
-    }
+    };
 }
 
 type Props = {
     onChangeLocation: (location: OrderLocationData) => void;
 }
 
-function OrderLocation({ onChangeLocation }: Props) {
+function OrderLocation({onChangeLocation}: Props) {
     const [address, setAddress] = useState<Place>({
         position: initialPosition
     });
 
     const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
         const response = await fetchLocalMapBox(inputValue);
-
+      
         const places = response.data.features.map((item: any) => {
-            return ({
-                label: item.place_name,
-                value: item.place_name,
-                position: {
-                    lat: item.center[1],
-                    lng: item.center[0]
-                }
-            });
+          return ({
+            label: item.place_name,
+            value: item.place_name,
+            position: {
+              lat: item.center[1],
+              lng: item.center[0]
+            }
+          });
         });
-
+      
         callback(places);
-    };
-
-    const handleChangeSelect = (place: Place) => {
+      };
+      
+      const handleChangeSelect = (place: Place) => {
         setAddress(place);
-        onChangeLocation({
+          onChangeLocation({
           latitude: place.position.lat,
           longitude: place.position.lng,
           address: place.label!
         });
-    };
+      };
 
     return (
         <div className="order-location-container">
@@ -59,7 +59,7 @@ function OrderLocation({ onChangeLocation }: Props) {
                 <h3 className="order-location-title">
                     Selecione onde o pedido deve ser entregue:
                 </h3>
-                <div className="filer-container">
+                <div className="filter-container">
                     <AsyncSelect
                         placeholder="Digite um endereço para entregar o pedido"
                         className="filter"
@@ -67,23 +67,23 @@ function OrderLocation({ onChangeLocation }: Props) {
                         onChange={value => handleChangeSelect(value as Place)}
                     />
                 </div>
+                <MapContainer
+                    center={address.position}
+                    zoom={15}
+                    key={address.position.lat}
+                    scrollWheelZoom
+                    >
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={address.position}>
+                        <Popup>
+                            {address.label}
+                        </Popup>
+                    </Marker>
+                </MapContainer>
             </div>
-            <MapContainer
-                center={address.position}
-                zoom={15}
-                key={address.position.lat}
-                scrollWheelZoom
-                >
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={address.position}>
-                    <Popup>
-                        {address.label}
-                    </Popup>
-                </Marker>
-            </MapContainer>
         </div>
     )
 }
